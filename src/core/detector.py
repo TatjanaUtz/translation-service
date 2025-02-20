6facf7eeea312751f48aa7e99516176a6c698ed9
Tatjana Utz <t.utz@cadenas.de>
2025-02-20 09:32:38 +0100

    Refactor language detector to remove redundant error checks and streamline initialization

--- src/core/detector.py
+++ src/core/detector.py
@@ -5,8 +5,6 @@ This module provides a class for detecting the language of a given text using th
 
 from lingua import Language, LanguageDetectorBuilder
 
-from exceptions import DetectionError, DetectorInitializationError
-
 
 class Detector:
     """A class used to detect the language of a given text."""
@@ -21,18 +19,10 @@ class Detector:
             ValueError: If the supported_languages list is empty.
             DetectorInitializationError: If the language detector fails to initialize.
         """
-        if not supported_languages:
-            msg = "supported_languages list cannot be empty"
-            raise ValueError(msg)
-
         self.supported_languages = supported_languages
         self.detector = (
             LanguageDetectorBuilder.from_languages(*supported_languages).with_preloaded_language_models().build()
         )
-
-        if not self.detector:
-            msg = "Failed to initialize the language detector."
-            raise DetectorInitializationError(msg)
 
     def detect_language(self, text: str) -> str:
         """Detects the language of the given text.
@@ -47,14 +37,5 @@ class Detector:
             ValueError: If the input text is empty or contains only whitespace.
             DetectionError: If the language of the given text could not be detected.
         """
-        if not text.strip():
-            msg = "text cannot be empty"
-            raise ValueError(msg)
-
         detected_language = self.detector.detect_language_of(text)
-
-        if not detected_language:
-            msg = "Could not detect the language of the given text."
-            raise DetectionError(msg)
-
         return str(detected_language.iso_code_639_1.name.lower())
